87a0555d265f75a4f026ac0f0993bcf3b5f9d422
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-10-18 04:41:23 +0000

    First

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import Project from "./Project";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>To-Do List</h1>
+        <Project />
     </div>
   )
 }
